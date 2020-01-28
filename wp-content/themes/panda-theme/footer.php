297a9d97ad1e9f3fad07266e4659b3fdf1855157
mkeyy <mk.websitesdev@gmail.com>
2020-01-28 18:22:58 +0100

    copyrights 2020

--- wp-content/themes/panda-theme/footer.php
+++ wp-content/themes/panda-theme/footer.php
@@ -8,7 +8,7 @@
 
     <footer class="pt-footer">
         <div class="pt-site-info">
-            <span class="pt-copyrights"><?= __('&copy; Copyrights 2019 - Konrad Klimczak', 'panda-theme') ?></span>
+            <span class="pt-copyrights"><?= __('&copy; Copyrights 2020 - Konrad Klimczak', 'panda-theme') ?></span>
         </div>
     </footer>
 </main><!-- main end -->
